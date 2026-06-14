bd71cb59f1ef652dcfca40938fac03606bc54233
MaydayV <61279703+MaydayV@users.noreply.github.com>
2026-06-14 16:46:34 +0800

    fix: 修复新版 GitHub 搜索弹层关闭问题
    - 隔离新版 React 搜索组件的文本翻译
    - 避免 DOM 改写触发搜索弹层卸载

--- chrome/locals.js
+++ chrome/locals.js
@@ -107,6 +107,7 @@ I18N.conf = {
         '*': [
             'div.QueryBuilder-StyledInputContainer', // 顶部搜索栏 关键词
             '#qb-input-query span', // 搜索页面 搜索栏 关键词
+            '[class*="Search-module__"]', // 顶部新版 React 搜索组件，改写内部文本会导致弹层关闭
 			'div.styled-input-content', // 筛选条
         ],
     },
@@ -232,6 +233,7 @@ I18N.conf = {
             '.comment-body', '.js-preview-body',
             '.markdown-title',
             'span.ActionListItem-descriptionWrap',  // 顶部搜索栏 关键词
+            '[class*="Search-module__"]', // 顶部新版 React 搜索组件
             'CODE', 'SCRIPT', 'STYLE', 'LINK', 'IMG', 'MARKED-TEXT', 'PRE', 'KBD', 'SVG', 'MARK', // 特定元素标签
 			'div.styled-input-content', // 筛选条
         ],
